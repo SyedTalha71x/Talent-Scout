4d22131f8bbf3bc8549f9cee9019acb7969104dd
SyedTalha71x <syedtalha71x@gmail.com>
2024-09-13 06:12:58 +0000

    updates on server

--- src/app/Components/Partials/DashboardPartials/CreateJob/page.tsx
+++ src/app/Components/Partials/DashboardPartials/CreateJob/page.tsx
@@ -1,3 +1,4 @@
+"use client";
 import React from 'react';
 import { Form, Input, Button, Select, InputNumber, DatePicker, Checkbox, Row, Col, Collapse, message } from 'antd';
 import ReactQuill from 'react-quill'; // Import ReactQuill
