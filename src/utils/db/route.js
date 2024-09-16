9728197b575fdf7b2a6fde5bd3a0a09a0246e5be
SyedTalha71x <syedtalha71x@gmail.com>
2024-09-16 11:12:38 +0000

    fix updates

--- src/utils/db/route.js
+++ src/utils/db/route.js
@@ -1,22 +1,15 @@
-// utils/db/connection.js
 import mongoose from "mongoose";
 
-let isConnected = false;
+const MONGODB_KEY = 'mongodb://localhost:27017/DevTalent';
 
 export async function connectToDB() {
-    if (isConnected) {
-        console.log("Already connected to MongoDB");
-        return;
+    try{
+        await mongoose.connect(MONGODB_KEY);
+        console.log("Connected with MongoDB Successfull")
+
     }
-    try {
-        await mongoose.connect(process.env.MONGO_DB_URI, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        });
-        isConnected = true;
-        console.log("Connected with MongoDB Successfully");
-    } catch (error) {
-        console.error("Failed to connect with MongoDB", error);
-        throw new Error("Failed to connect with MongoDB");
+    catch(error){
+        console.log("Failed to connect with mongodb", error);
     }
+    
 }
