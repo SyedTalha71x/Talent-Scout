4310b545b3e1b76f8214f248111f766da7f75ccc
SyedTalha71x <syedtalha71x@gmail.com>
2024-10-31 10:18:41 +0000

    some updates

--- src/app/api/Jobs/getJob/route.ts
+++ src/app/api/Jobs/getJob/route.ts
@@ -1,25 +1,16 @@
 import Job from "@/utils/Models/job-model";
 import { connectToDB } from "@/utils/db/route";
 import { NextRequest, NextResponse } from "next/server";
-import { cors } from '@/lib/cors'
 
 export const GET = async (request: any) => {
-    const corsHeaders = cors(request)
   
     try {
         await connectToDB();
         const fetchdata = await Job.find();
-        return NextResponse.json({ jobs: fetchdata,  headers: corsHeaders })
+        return NextResponse.json({ jobs: fetchdata})
     }
     catch (error: any) {
         console.log(error);
         return NextResponse.json({ error: 'Internal Server Error' }, { status: 500 })
     }
 }
-
-
-export async function OPTIONS(request: NextRequest) {
-    const corsHeaders = cors(request)
-    return NextResponse.json({}, { headers: corsHeaders })
-  }
-
